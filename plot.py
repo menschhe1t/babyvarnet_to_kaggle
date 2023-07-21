0c158e9742cc16b4a8287f617feb944a5e85e1a1
menschhe1t <135943267+menschhe1t@users.noreply.github.com>
2023-07-21 19:11:55 +0900

    Update plot.py

--- plot.py
+++ plot.py
@@ -1,8 +1,7 @@
 import h5py
 import matplotlib.pyplot as plt
 
-#f = h5py.File('/kaggle/working/result/test_Unet/reconstructions_val/brain_acc4_179.h5', 'r')
-f = h5py.File('/kaggle/working/result/test_Unet/reconstructions_leaderboard/acc8/brain_test54.h5', 'r')
+f = h5py.File('/kaggle/working/result/test_Unet/reconstructions_val/brain_acc4_179.h5', 'r')
 
 print(f)
 input = f['input']
