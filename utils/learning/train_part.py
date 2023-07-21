0bf5da7b7560373ccf6a32446de675701d463d00
menschhe1t <135943267+menschhe1t@users.noreply.github.com>
2023-07-21 22:01:28 +0900

    Update train_part.py

--- utils/learning/train_part.py
+++ utils/learning/train_part.py
@@ -129,7 +129,7 @@ def save_model(args, exp_dir, epoch, model, optimizer, best_val_loss, is_new_bes
             'exp_dir': exp_dir
         },
         # f=exp_dir / 'model.pt'
-        f=exp_dir / 'best_model.pt'
+        f=exp_dir / f'epoch{epoch}_bestvalloss{best_val_loss}_best_model.pt'
     )
     # if is_new_best:
     #     shutil.copyfile(exp_dir / 'model.pt', exp_dir / 'best_model.pt')
