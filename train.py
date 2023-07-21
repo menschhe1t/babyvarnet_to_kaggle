8037a8e1cf506f5a2a7a56efd4ad48e68cc7e48d
menschhe1t <135943267+menschhe1t@users.noreply.github.com>
2023-07-21 23:02:19 +0900

    Update train.py

--- train.py
+++ train.py
@@ -14,9 +14,9 @@ def parse():
     parser = argparse.ArgumentParser(description='Train Unet on FastMRI challenge Images',
                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-g', '--GPU-NUM', type=int, default=0, help='GPU number to allocate')
-    parser.add_argument('-b', '--batch-size', type=int, default=32, help='Batch size')
-    parser.add_argument('-e', '--num-epochs', type=int, default=30, help='Number of epochs')
-    parser.add_argument('-l', '--lr', type=float, default=1e-3, help='Learning rate')
+    parser.add_argument('-b', '--batch-size', type=int, default=34, help='Batch size')
+    parser.add_argument('-e', '--num-epochs', type=int, default=25, help='Number of epochs')
+    parser.add_argument('-l', '--lr', type=float, default=5e-4, help='Learning rate')
     parser.add_argument('-r', '--report-interval', type=int, default=5, help='Report interval')
     parser.add_argument('-n', '--net-name', type=Path, default='test_Unet', help='Name of network')
     parser.add_argument('-t', '--data-path-train', type=Path, default='/kaggle/input/fmrikaggle2try/2023_snu_fastmri_dataset_onlyimage/train/image/', help='Directory of train data')
