a4b4da4368780c859e4be7d723a71227deb33f7f
menschhe1t <135943267+menschhe1t@users.noreply.github.com>
2023-07-21 19:18:53 +0900

    Update plot.py

--- plot.py
+++ plot.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 
 f = h5py.File('/kaggle/working/result/test_Unet/reconstructions_val/brain_acc4_179.h5', 'r')
 
-print(f)
 input = f['input']
 recon = f['reconstruction']
 target = f['target']
