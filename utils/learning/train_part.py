05dbf4b14fcd6fd421746cbf3b9b43873d5b373a
menschhe1t <135943267+menschhe1t@users.noreply.github.com>
2023-07-23 01:08:22 +0900

    Update train_part.py

--- utils/learning/train_part.py
+++ utils/learning/train_part.py
@@ -164,10 +164,10 @@ def train(args):
     # train_transform = get_train_transform()
     # valid_transform = get_valid_transform()
     
-    #input_train_loader, grappa_train_loader = create_data_loaders(data_path = args.data_path_train,mode = 'train' ,args = args, shuffle=True)
-    input_train_loader = create_data_loaders(data_path = args.data_path_train,mode = 'train' ,args = args, shuffle=True)
-    #input_val_loader, grappa_val_loader = create_data_loaders(data_path = args.data_path_val,mode = 'valid' ,args = args)
-    input_val_loader = create_data_loaders(data_path = args.data_path_val,mode = 'valid' ,args = args)
+    input_train_loader, grappa_train_loader = create_data_loaders(data_path = args.data_path_train,mode = 'train' ,args = args, shuffle=True)
+    #input_train_loader = create_data_loaders(data_path = args.data_path_train,mode = 'train' ,args = args, shuffle=True)
+    input_val_loader, grappa_val_loader = create_data_loaders(data_path = args.data_path_val,mode = 'valid' ,args = args)
+    #input_val_loader = create_data_loaders(data_path = args.data_path_val,mode = 'valid' ,args = args)
     
     val_loss_log = np.empty((0, 2))
     
