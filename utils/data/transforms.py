1c4d96fc41bba0fe3549f67a7ac02c29fb5eb49b
menschhe1t <135943267+menschhe1t@users.noreply.github.com>
2023-07-22 17:32:16 +0900

    Update transforms.py

--- utils/data/transforms.py
+++ utils/data/transforms.py
@@ -47,10 +47,10 @@ class DataTransform:
             return input, target, maximum, fname, slice
         
         elif self.mode == 'test':
-            input = cv2.resize(input[:,:, np.newaxis], (346,346))
+            input = cv2.resize(input[:,:, np.newaxis], (384,384))
             if not self.isforward:
                 # target = to_tensor(target)
-                target = cv2.resize(target[:,:, np.newaxis], (346,346))
+                target = cv2.resize(target[:,:, np.newaxis], (384,384))
                 target = torch.squeeze(torch.tensor(target))
                 maximum = attrs[self.max_key]
             else:
