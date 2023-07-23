c60a9f92e638b4749e268f3b2a4fb6e9dafb5d5d
menschhe1t <135943267+menschhe1t@users.noreply.github.com>
2023-07-23 18:40:22 +0900

    Update load_data.py

--- utils/data/load_data.py
+++ utils/data/load_data.py
@@ -5,7 +5,7 @@ from torch.utils.data import Dataset, DataLoader
 from pathlib import Path
 from pathlib import PosixPath
 
-class SliceData(Dataset):
+class train_SliceData(Dataset):
     def __init__(self, root, transform, input_key, grappa_key, target_key, data_type, forward=False):
         self.transform = transform
         self.input_key = input_key
@@ -80,6 +80,46 @@ class SliceData(Dataset):
 
 
 
+
+class test_SliceData(Dataset):
+    def __init__(self, root, transform, input_key, target_key, forward=False):
+        self.transform = transform
+        self.input_key = input_key
+        self.target_key = target_key
+        self.forward = forward
+        self.examples = []
+        
+        files = list(Path(root).iterdir())
+        for fname in sorted(files):
+            num_slices = self._get_metadata(fname)
+
+            self.examples += [
+                (fname, slice_ind) for slice_ind in range(num_slices)
+            ]
+
+    def _get_metadata(self, fname):
+        
+        with h5py.File(fname, "r") as hf:
+            num_slices = hf[self.input_key].shape[0]
+        return num_slices
+
+    def __len__(self):
+        return len(self.examples)
+
+    def __getitem__(self, i):
+        fname, dataslice = self.examples[i]
+        
+        with h5py.File(fname, "r") as hf:
+            input = hf[self.input_key][dataslice]
+            if self.forward:
+                target = -1
+            else:
+                target = hf[self.target_key][dataslice]
+            attrs = dict(hf.attrs)
+            
+        return self.transform(input, target, attrs, fname.name, dataslice), 
+
+
 def create_data_loaders(data_path, mode, args,  data_type, shuffle=False, isforward=False ):
     if isforward == False:
         max_key_ = args.max_key
@@ -88,7 +128,7 @@ def create_data_loaders(data_path, mode, args,  data_type, shuffle=False, isforw
         max_key_ = -1
         target_key_ = -1
     if mode != 'test':
-        data_storage = SliceData(
+        data_storage = train_SliceData(
             root=data_path,
             transform=DataTransform(isforward, max_key_, mode),
             input_key=args.input_key,
@@ -98,14 +138,14 @@ def create_data_loaders(data_path, mode, args,  data_type, shuffle=False, isforw
             forward = isforward
         )
     else :
-        data_storage = SliceData(
+        data_storage = test_SliceData(
             root=data_path,
             transform=DataTransform(isforward, max_key_, mode),
             input_key=args.input_key,
             target_key=target_key_,
-            data_type = data_type,
             forward = isforward
         )
+    
     data_loader = DataLoader(
         dataset= data_storage,
         batch_size=args.batch_size,
