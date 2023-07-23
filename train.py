e78a21e8e2878e01fe53aac17e00cd9825f9ce06
menschhe1t <135943267+menschhe1t@users.noreply.github.com>
2023-07-23 20:01:28 +0900

    Update train.py

--- train.py
+++ train.py
@@ -17,7 +17,7 @@ def parse():
     parser.add_argument('-w', '--num-workers', type=int, default=2, help='Num Workers')
     parser.add_argument('-b', '--batch-size', type=int, default=36, help='Batch size') #size : 1200
     #parser.add_argument('-b', '--batch-size', type=int, default=400, help='Batch size') #size : 800
-    parser.add_argument('-e', '--num-epochs', type=int, default=30, help='Number of epochs')
+    parser.add_argument('-e', '--num-epochs', type=int, default=35, help='Number of epochs')
     parser.add_argument('-l', '--lr', type=float, default=1e-3, help='Learning rate')
     parser.add_argument('-r', '--report-interval', type=int, default=5, help='Report interval')
     parser.add_argument('-n', '--net-name', type=Path, default='test_Unet', help='Name of network')
